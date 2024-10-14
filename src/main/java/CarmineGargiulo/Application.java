57d402bc532e489d16bada1a3eb181b8b6f42e6b
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-10-14 11:02:57 +0200

    initial commit

---
+++ src/main/java/CarmineGargiulo/Application.java
@@ -0,0 +1,15 @@
+package CarmineGargiulo;
+
+import com.github.javafaker.Faker;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Locale;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("azienda-trasporti");
+    private static final Faker faker = new Faker(Locale.US);
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
