bf176bd96e69b898e83468d3a0ec1a64f14b0be9
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-10-15 12:50:26 +0200

    titoloViaggioDao id

---
+++ src/main/java/CarmineGargiulo/dao/TitoloViaggioDao.java
@@ -0,0 +1,37 @@
+package CarmineGargiulo.dao;
+
+import CarmineGargiulo.entities.Tessera;
+import CarmineGargiulo.entities.TitoloViaggio;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+import java.util.UUID;
+
+public class TitoloViaggioDao {
+    private final EntityManager entityManager;
+
+    public TitoloViaggioDao(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    public void salvaTitoloViaggio(TitoloViaggio titoloViaggio){
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(titoloViaggio);
+        transaction.commit();
+        System.out.println("Titolo viaggio " + titoloViaggio.getTitoloViaggio_id() + " salvato correttamente" );
+    }
+
+    public List<TitoloViaggio> ottieniListaTessere(){
+        TypedQuery<TitoloViaggio> query = entityManager.createNamedQuery("getAllTitoliViaggio", TitoloViaggio.class);
+        return query.getResultList();
+    }
+
+    public TitoloViaggio findTitoloViaggioById(String id){
+        TitoloViaggio cercato = entityManager.find(TitoloViaggio.class, UUID.fromString(id));
+        if(cercato == null) throw new RuntimeException(); //TODO creare eccezione
+        return cercato;
+    }
+}
